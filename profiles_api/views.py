970e17e4bd733336aae7e913f773981479b4f782
Ziad Mossad <z.mossad@transtecbds.com>
2025-08-23 17:45:40 +0300

    added hello view api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Return a list"""
+
+        get_list = ["a", "b", "c", "d"]
+        return Response({"message": "Hello!", "get_list": get_list})
